0f3464a99473f9cd28c0a93aa54c042871fea424
zone12138 <1459547902@qq.com>
2026-02-02 22:31:31 +0800

    feat: 未完成版heatmap

--- src/threejs/components/Heatmap.js
+++ src/threejs/components/Heatmap.js
@@ -1,7 +1,24 @@
 import h337 from 'heatmap.js'
 import merge from 'lodash-es/merge'
 
-const HEATMAP_DEFAULT_OPTS = {}
+const HEATMAP_DEFAULT_OPTS = {
+  projection: {
+    center: [0, 0],
+    scale: 1,
+    translate: [0, 0],
+  },
+  gradient: {
+    0.5: '#1fc2e1',
+    0.6: '#24d560',
+    0.7: '#9cd522',
+    0.8: '#f1e12a',
+    0.9: '#ffbf3a',
+    1.0: '#ff0000',
+  },
+  blur: 1,
+  radius: 10,
+  maxOpacity: 1,
+}
 
 export class Heatmap {
   constructor({ scene, sizes }, opts = {}) {
@@ -14,6 +31,8 @@ export class Heatmap {
     this.width = this.sizes.width ?? 1024
     this.height = this.sizes.height ?? 1024
 
+    const { gradient, blur, radius, maxOpacity } = this.opts
+
     this.dom = document.createElement('div')
     this.dom.style.width = `${this.width}px`
     this.dom.style.height = `${this.height}px`
@@ -23,8 +42,18 @@ export class Heatmap {
 
     this.instance = h337.create({
       container: this.dom,
-      radius: 10,
-      blur: 3,
+      gradient,
+      blur,
+      radius,
+      maxOpacity,
+    })
+
+    const greymap = h337.create({
+      container: this.dom,
+      gradient: {
+        0.0: 'black',
+        1.0: 'white',
+      },
     })
   }
 }
